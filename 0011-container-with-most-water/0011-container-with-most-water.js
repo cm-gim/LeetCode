24630420910fe32ef5c6aa462d4be649e8f471aa
cm-gim <117872640+cm-gim@users.noreply.github.com>
2025-03-20 18:53:41 +0900

    Time: 2 ms (80.37%), Space: 61.8 MB (39.96%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -1,21 +1,3 @@
-// /**
-//  * @param {number[]} height
-//  * @return {number}
-//  */
-// const maxArea = function(height) {
-//     let lp = 0, rp = 1, answer = 0;
-//     while(lp < height.length -1){
-//         const h = Math.min(height[lp], height[rp]);
-//         const l = rp - lp
-//         answer = Math.max(answer, h*l);
-//         if(rp < height.length - 1) rp += 1;
-//         else {
-//             lp += 1;
-//             rp = lp + 1;
-//         }
-//     }
-//     return answer
-// };
 /**
  * @param {number[]} height
  * @return {number}
